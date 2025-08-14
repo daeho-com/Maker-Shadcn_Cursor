f5b536e5564b8eef714af28360ef72460388a475
daeho-com <younyenho@naver.com>
2025-08-14 18:26:10 +0900

    Submit 페이지 Icon 업로드 제작

--- app/features/products/pages/submit-page.tsx
+++ app/features/products/pages/submit-page.tsx
@@ -4,6 +4,10 @@ import { Hero } from "~/common/components/hero";
 import { Form } from "react-router";
 import InputPair from "~/common/components/input-pair";
 import SelectPair from "~/common/components/select-pair";
+import { Input } from "~/common/components/ui/input";
+import { Label } from "~/common/components/ui/label";
+import { useState } from "react";
+import { Button } from "~/common/components/ui/button";
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -14,12 +18,19 @@ export const meta: Route.MetaFunction = () => {
 
 
 export default function SubmitPage({actionData}: Route.ComponentProps) {
+  const [icon, setIcon] = useState<string | null>(null);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      setIcon(URL.createObjectURL(file));
+    }
+  }
   return (
     <div className="space-y-20">
       <Hero 
         title="Submit Your Product" 
         subtitle="Submit your product with the world"/>
-      <Form className = "grid grid-cols-2 gap-1 max-w-screen-lg mx-auto">
+      <Form className = "grid grid-cols-2 gap-10 max-w-screen-lg mx-auto">
         <div className="space-y-5"> 
           <InputPair 
             label="Product Name" 
@@ -67,8 +78,26 @@ export default function SubmitPage({actionData}: Route.ComponentProps) {
               {label: "Design", value: "design"},
               {label: "Development", value: "development"},
             ]} />
+          <Button type = "submit" className = "w-full size-lg">Submit</Button>
+        </div>  
+        <div className ="flex flex-col space-y-2">
+          <div className = "size-40 rounded-xl shadow-xl overflow-hidden">
+            {icon ? 
+                <img src = {icon} alt = "icon" className = "size-full object-cover" />
+              : null}
+          </div>
+          <Label className = "flex flex-col gap-1px items-start">
+            Icon
+            <small className="text-muted-foreground">The icon of your product</small>
+            </Label>
+          <Input type = "file" className = "w-1/2" onChange = {onChange} required name = "icon" />
+          <div className = "flex flex-col gap-1px text-xs">
+            <span className = "text-muted-foreground">Recommend size : 128x128px</span>
+            <span className = "text-muted-foreground">Allowed format : PNG</span>
+            <span className = "text-muted-foreground">Max file size : 1MB</span>
+          </div>
         </div>
       </Form>
-    </div> 
+    </div>  
   );
 }

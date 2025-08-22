36cdefe86466c5d386ce9f8ce81f83ed384039b8
daeho-com <younyenho@naver.com>
2025-08-22 10:37:28 +0900

    Magic UI 라이브러리 사용하여 Login 페이지 UI 꾸미기
    - FlickeringGrid 컴포넌트 추가 및 Auth 레이아웃 적용
    - 그리드 애니메이션 색상, 크기 커스터마이징
    - UI 컴포넌트 폴더 구조 정리

--- app/features/auth/layouts/auth-layout.tsx
+++ app/features/auth/layouts/auth-layout.tsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router";
+import { FlickeringGrid } from "~/common/components/ui/flickering-grid";
 
 export default function AuthLayout() {
   return (
     <div className = "grid grid-cols-1 lg:grid-cols-2 h-screen">
-        <div className = "bg-gradient-to-br from-primary hidden lg:block via-black to-primary/50">
+        <div className = "hidden lg:block">
+            <FlickeringGrid 
+              squareSize={4} 
+              gridGap={5} 
+              flickerChance={0.2} 
+              maxOpacity={0.5}
+              color="#E11D48" 
+            />
         </div>
         <Outlet/>
     </div>
